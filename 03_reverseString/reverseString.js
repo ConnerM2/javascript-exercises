c52239d5c9568901285cd0222e87398aa07c92ef
Conner Manwill <140928079+ConnerM2@users.noreply.github.com>
2024-08-09 16:48:26 -0600

    TOP exercises 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
-};
+let str = 'Hello World'
+const reverseString = function(str) {
+    let arr = str.split('')
+    let reversedArr = []
+    for(let i = 0; i < arr.length; i++) {
+        reversedArr.unshift(arr[i])
+    }
+    let reversedStr = reversedArr.join('')
+    return reversedStr
+}
+console.log(reverseString(str))
 
 // Do not edit below this line
 module.exports = reverseString;
