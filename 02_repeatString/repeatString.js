5f864013191b3b43b983220ec56a66b4035ab21c
Conner Manwill <140928079+ConnerM2@users.noreply.github.com>
2024-08-09 16:49:43 -0600

    TOP excersises 1-2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeat = '';
+    for (let i = 0; i < num; i++) {
+        repeat += string
+    }
+    return repeat
 };
 
+console.log(repeatString('hello', 4))
 // Do not edit below this line
 module.exports = repeatString;
