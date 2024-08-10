d0b55c9759219a8d5d0e0800da0e13f4ab430fd9
Conner Manwill <140928079+ConnerM2@users.noreply.github.com>
2024-08-09 18:26:59 -0600

    TOP 4-5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+let arr = ([1, 2, 3, 4])
 
+const removeFromArray = function (array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    return newArray;
+  };
+
+console.log(removeFromArray(arr, 3))
 // Do not edit below this line
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0
+
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR'
+    } else if (typeof num1 != 'number' || typeof num2 != 'number') {
+        return 'ERROR'
+    } else if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            sum += i
+        }
+        return sum
+    } else if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            sum += i
+        }
+        return sum
+    }
 
 };
 
+console.log(sumAll(60, 3))
+
+//Implement a function that takes 2 positive integers and returns the sum of every integer between (and including) them:
 // Do not edit below this line
-module.exports = sumAll;
+//module.exports = sumAll;
